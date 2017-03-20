51a11af2d9f7b228fe931716d58760c3e1180c68
zyz <272296321@qq.com>
2017-03-20 22:44:07 +0800

    添加初始化用户信息

--- src/main/java/com/zyz/server/service/UserService.java
+++ src/main/java/com/zyz/server/service/UserService.java
@@ -6,16 +6,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sun.misc.BASE64Encoder;
 
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.List;
-
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 
 public class UserService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
+
+	private static final Map<Long,User> USERMAP = new TreeMap<Long,User>();
 
 	public boolean createUser(User user)
 	{
@@ -31,6 +34,7 @@ public class UserService {
 	}
 	public List<User> queryUsers(String userNamePrex,boolean onlyValidUser)
 	{
+
 		return null;
 	}
 	/**
@@ -55,8 +59,16 @@ public class UserService {
 			LOGGER.error("userName=>{} is existed",userName);
 			return userSession;
 		}
-		if(null==user.getPassword()){
-
+		try{
+			if(encoderByMd5(user.getPassword()).equals(md5EncodedPassword)){
+				userSession.setCreateTime(System.currentTimeMillis());
+				userSession.setSessionId(UUID.randomUUID().toString());
+				userSession.setUserId(user.getUserId());
+				userSession.setUserName(user.getUserName());
+				userSession.setValidSeconds((short) (30*60));
+			}
+		}catch (Exception e){
+			LOGGER.error("check passWord error:",e);
 		}
 		return userSession;
 	}
@@ -65,17 +77,59 @@ public class UserService {
 		return null;
 	}
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws Exception{
+		Map<Long,User> userMap = reloadUsers();
+		LOGGER.info("info");
 	}
 
-	public String EncoderByMd5(String str) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+	private String encoderByMd5(String str) throws NoSuchAlgorithmException, UnsupportedEncodingException {
 	    //确定计算方法
- 　　　 MessageDigest md5= MessageDigest.getInstance("MD5");
-　　　　BASE64Encoder base64en = new BASE64Encoder();
+		MessageDigest md5= MessageDigest.getInstance("MD5");
+		BASE64Encoder base64en = new BASE64Encoder();
         //加密后的字符串
-　　　　String newstr=base64en.encode(md5.digest(str.getBytes("utf-8")));
- 　　　 return newstr;
-　　}
+		String newstr=base64en.encode(md5.digest(str.getBytes("utf-8")));
+		return newstr;
+	}
+
+
+	public static Map<Long,User> reloadUsers(){
+		Map<Long,User> userMap = new TreeMap<Long, User>();
+		File userFile = new File("./user.txt");
+		if(null==userFile||!userFile.exists()){
+			return null;
+		}
+		BufferedReader reader = null;
+		try{
+			reader = new BufferedReader(new FileReader(userFile));
+			String tempString = null;
+			String[] userArrayStr = null;
+			// 一次读入一行，直到读入null为文件结束
+			while ((tempString = reader.readLine()) != null) {
+				userArrayStr = tempString.split("|");
+				User user = new User();
+				user.setUserName(userArrayStr[1]);
+				user.setUserId(Long.valueOf(userArrayStr[0]));
+				user.setEnabled(Boolean.valueOf(userArrayStr[3]));
+				user.setPassword(userArrayStr[2]);
+
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+				user.setRegDate(sdf.parse(userArrayStr[4]));
+				userMap.put(Long.valueOf(userArrayStr[0]),user);
+			}
+			reader.close();
+		}catch (Exception e){
+			LOGGER.error("reload users error:",e);
+		}finally {
+			if(null!=reader){
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return userMap;
+	}
 
 }
