00dc2fa08ced2e6c3731b7a83d681ac2222f7650
zyz <272296321@qq.com>
2017-03-28 18:11:20 +0800

    添加seesion

--- src/main/java/com/zyz/server/service/UserService.java
+++ src/main/java/com/zyz/server/service/UserService.java
@@ -9,7 +9,6 @@ import sun.misc.BASE64Encoder;
 import java.io.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -18,25 +17,69 @@ public class UserService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
 
-	private static final Map<Long,User> USERMAP = new TreeMap<Long,User>();
+	private static volatile Map<Long,User> USERCACHE = new TreeMap<Long,User>();
+
+	private static volatile Map<Long,UserSession> SESSIONMAP = new HashMap<Long, UserSession>();
+
+	private static final SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+	private static final String SPLITSTRING = "#";
 
 	public boolean createUser(User user)
 	{
+		File userFile = null;
+		try{
+			userFile = new File("./user.txt");
+			if(null==userFile||!userFile.exists()){
+				LOGGER.warn("user resource不存在,已创建");
+				userFile.createNewFile();
+				userFile = new File("./user.txt");
+			}
+			StringBuilder userBuilder = new StringBuilder();
+			this.fillUserString(user,userBuilder);
+			this.saveFile(userBuilder.toString(),userFile,true);
+			USERCACHE.put(user.getUserId(),user);
+		}catch (Exception e){
+			LOGGER.error("create users error:",e);
+			USERCACHE.remove(user.getUserId());
+			return false;
+		}
 		return true;
 	}
+
 	public boolean deleteUser(long userId)
 	{
+		try{
+			USERCACHE.remove(userId);
+		}catch (Exception e){
+			LOGGER.error("deleteUser error:",e);
+		}
 		return true;
 	}
+
 	public boolean disableUser(long userId)
 	{
+		try{
+			User user = USERCACHE.get(userId);
+			user.setEnabled(false);
+		}catch (Exception e){
+			LOGGER.error("disableUser error:",e);
+		}
 		return true;
 	}
+
 	public List<User> queryUsers(String userNamePrex,boolean onlyValidUser)
 	{
 
-		return null;
+		List<User> list = new ArrayList<User>();
+		for(User user : USERCACHE.values()){
+			if(user.getUserName().startsWith(userNamePrex)&&user.isEnabled()==onlyValidUser){
+				list.add(user);
+			}
+		}
+		return list;
 	}
+
 	/**
 	 * 如果密码不对，返回的UserSession对象里sessionId为空，客户端可以依次判断，参照UserSession.isValid方法
 	 * @param userName
@@ -61,11 +104,17 @@ public class UserService {
 		}
 		try{
 			if(encoderByMd5(user.getPassword()).equals(md5EncodedPassword)){
-				userSession.setCreateTime(System.currentTimeMillis());
-				userSession.setSessionId(UUID.randomUUID().toString());
-				userSession.setUserId(user.getUserId());
-				userSession.setUserName(user.getUserName());
-				userSession.setValidSeconds((short) (30*60));
+				if(!SESSIONMAP.containsKey(user.getUserId())){
+					userSession.setCreateTime(System.currentTimeMillis());
+					userSession.setSessionId(UUID.randomUUID().toString());
+					userSession.setUserId(user.getUserId());
+					userSession.setUserName(user.getUserName());
+					userSession.setValidSeconds((short) (30*60));
+					SESSIONMAP.put(user.getUserId(),userSession);
+				}else{
+					userSession = SESSIONMAP.get(user.getUserId());
+					LOGGER.info("userId=>{} already login",user.getUserId());
+				}
 			}
 		}catch (Exception e){
 			LOGGER.error("check passWord error:",e);
@@ -74,12 +123,14 @@ public class UserService {
 	}
 
 	public User getUser(String userName){
-		return null;
-	}
-
-	public static void main(String[] args) throws Exception{
-		Map<Long,User> userMap = reloadUsers();
-		LOGGER.info("info");
+		User user = null;
+		for(User temp : USERCACHE.values()){
+			if(temp.getUserName().equals(userName)){
+				user = temp;
+				break;
+			}
+		}
+		return user;
 	}
 
 	private String encoderByMd5(String str) throws NoSuchAlgorithmException, UnsupportedEncodingException {
@@ -91,12 +142,74 @@ public class UserService {
 		return newstr;
 	}
 
+	public void reWrite(){
+		File userFile = new File("../user.txt");
+		if(USERCACHE.isEmpty()){
+			userFile.deleteOnExit();
+			LOGGER.info("缓存中不存在数据,清空文件");
+			return;
+		}
+		StringBuilder userString = new StringBuilder(1024);
+		for(User user : USERCACHE.values()){
+			fillUserString(user,userString);
+		}
+		saveFile(userString.toString(),userFile,false);
+	}
 
-	public static Map<Long,User> reloadUsers(){
+	/**
+	 * 把User对象数据填充到对应格式的字符串
+	 * @param user
+	 * @param userBuilder
+	 */
+	private void fillUserString (User user,StringBuilder userBuilder){
+		userBuilder.append(user.getUserId()).append(SPLITSTRING);
+		userBuilder.append(user.getUserName()).append(SPLITSTRING);
+		userBuilder.append(user.getPassword()).append(SPLITSTRING);
+		userBuilder.append(0).append(SPLITSTRING);
+		userBuilder.append(SDF.format(new Date()));
+		userBuilder.append("\r\n");
+	}
+
+	/**
+	 * 保存文件
+	 * @param str
+	 * @param file
+	 * @param isAppend  是否累加(在文件末尾添加)
+	 */
+	private void saveFile(String str,File file,boolean isAppend){
+		BufferedWriter writer = null;
+		try{
+			if(null==file||!file.exists()){
+				LOGGER.warn("file 不存在,已创建");
+				file.createNewFile();
+				file = new File("./user.txt");
+			}
+			writer = new BufferedWriter(new FileWriter(file,isAppend));
+			writer.append(str);
+			writer.close();
+		}catch (Exception e){
+			LOGGER.error(" saveFile error:",e);
+		}finally {
+			if(null!=writer){
+				try {
+					writer.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			//lock.unlock();
+		}
+	}
+
+	/**
+	 * 把文件的用户信息加载到缓存
+	 * @return
+	 */
+	public static void reloadUsersCache(){
 		Map<Long,User> userMap = new TreeMap<Long, User>();
 		File userFile = new File("./user.txt");
 		if(null==userFile||!userFile.exists()){
-			return null;
+			return;
 		}
 		BufferedReader reader = null;
 		try{
@@ -105,17 +218,14 @@ public class UserService {
 			String[] userArrayStr = null;
 			// 一次读入一行，直到读入null为文件结束
 			while ((tempString = reader.readLine()) != null) {
-				userArrayStr = tempString.split("|");
+				userArrayStr = tempString.split("#");
 				User user = new User();
 				user.setUserName(userArrayStr[1]);
 				user.setUserId(Long.valueOf(userArrayStr[0]));
 				user.setEnabled(Boolean.valueOf(userArrayStr[3]));
 				user.setPassword(userArrayStr[2]);
-
-				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-				user.setRegDate(sdf.parse(userArrayStr[4]));
-				userMap.put(Long.valueOf(userArrayStr[0]),user);
+				user.setRegDate(SDF.parse(userArrayStr[4]));
+				userMap.put(Long.valueOf(userArrayStr[0]), user);
 			}
 			reader.close();
 		}catch (Exception e){
@@ -129,7 +239,7 @@ public class UserService {
 				}
 			}
 		}
-		return userMap;
+		USERCACHE = userMap;
 	}
 
 }
